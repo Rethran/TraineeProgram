97a850fd2e97040adf36bdc9817875ea941b0538
Santeri <santtus97@hotmail.com>
2019-12-20 18:51:40 +0200

    File reader added
    file reader added and ready showing up mocking data on html page

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const http = require('http');
 const fs = require('fs');
+const pathURL = new URL("file:///C:/Users/Santeri/Desktop/traineeProgram/Mockdata/testdata/status.real");
 
 http.createServer(function(req, res) {
     // server code
     console.log(`${req.method} ${req.url}`);
-    res.end('hello world!');
-    false.readdir()
+
+    fs.readFile(pathURL, "utf8", (err, data) => {
+        if (!err) {
+            res.end(data);
+        } else {
+            console.log("errors: " + err)
+        }
+    })
 }).listen(9000);
 
 console.log('Server listening on port 9000');

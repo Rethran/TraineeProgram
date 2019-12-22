6c9454b4368446cd83ba299b3ccd8b3058e48116
Santeri <santtus97@hotmail.com>
2019-12-22 15:02:04 +0200

    Creating arrays+datasplit
    Created lines array which contains all lines of the package

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const http = require('http');
 const fs = require('fs');
-const pathURL = new URL("file:///C:/Users/Santeri/Desktop/traineeProgram/Mockdata/testdata/status.real");
+var lines = [];
+var packages = [];
 
+const pathURL = new URL("file:///C:/Users/Santeri/Desktop/traineeProgram/Mockdata/testdata/status.real");
 http.createServer(function(req, res) {
     // server code
     console.log(`${req.method} ${req.url}`);
 
     fs.readFile(pathURL, "utf8", (err, data) => {
+
         if (!err) {
+            lines = data.split('\n');
+            console.log(lines.length);
+            console.log(lines[1]);
             res.end(data);
+
+
+
         } else {
             console.log("errors: " + err)
         }
